ca05843e58f2157c3be2cf2d9380266cedb93866
fraten <59165445+fraten@users.noreply.github.com>
2020-01-17 20:21:28 +0100

    Update image.c

--- src/image.c
+++ src/image.c
@@ -332,8 +332,8 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
         int j;
         for (j = 0; j < classes; ++j) {
             if (selected_detections[i].det.prob[j] > thresh && j != best_class) {
-                printf("%s: %.0f%%", names[j], selected_detections[i].det.prob[j] * 100);
-                printf(".20%f %.20f %.20f %.20f\n", b.x, b.y, b.w, b.h);
+                printf("%s: %.0f%%\n", names[j], selected_detections[i].det.prob[j] * 100);
+                
             }
         }
     }
@@ -463,6 +463,7 @@ void draw_detections(image im, int num, float thresh, box *boxes, float **probs,
             if(top < 0) top = 0;
             if(bot > im.h-1) bot = im.h-1;
             printf("%s: %.0f%%", names[class_id], prob * 100);
+            printf(".20%f %.20f %.20f %.20f\n", b.x, b.y, b.w, b.h);
 
             //printf(" - id: %d, x_center: %d, y_center: %d, width: %d, height: %d",
             //    class_id, (right + left) / 2, (bot - top) / 2, right - left, bot - top);
